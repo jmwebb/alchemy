fc8d004ea669ccb5fc0429449057bcd60e63fd8c
jmwebb <jamesweb@usc.edu>
2018-03-24 00:38:29 -0700

    Basic element db, skeleton Particle class

---
+++ particle.py
@@ -0,0 +1,93 @@
+element_database = {
+	1: {'name': 'Phlogium', 'symbol': 'P', 'mass': 1., 'valence': 1, 'electronegativity':2.2, 'charges':{'magic': 10}}
+	,2: {'name': 'Vesuvium', 'symbol': 'V', 'mass': 4., 'valence': 18, 'electronegativity':0, 'charges':{'magic': 10}}
+	,3: {'name': 'Etherium', 'symbol': 'E', 'mass': 7., 'valence': 1, 'electronegativity':0.98, 'charges':{'magic': 10}}
+	,4: {'name': 'Smaragdium', 'symbol': 'Sm', 'mass': 9., 'valence': 2, 'electronegativity':1.53, 'charges':{'magic': 10}}
+	,5: {'name': 'Tinklium', 'symbol': 'T', 'mass': 11., 'valence': 13, 'electronegativity':2.04, 'charges':{'magic': 10}}
+	,6: {'name': 'Graphium', 'symbol': 'G', 'mass': 12., 'valence': 14, 'electronegativity':2.55, 'charges':{'magic': 10}}
+	,7: {'name': 'Mithrilium', 'symbol': 'M', 'mass': 14., 'valence': 15, 'electronegativity':3.04, 'charges':{'magic': 10}}
+	,8: {'name': 'Aerium', 'symbol': 'A', 'mass': 16., 'valence': 16, 'electronegativity':3.44, 'charges':{'magic': 10}}
+	,9: {'name': 'Fluxium', 'symbol': 'F', 'mass': 19., 'valence': 17, 'electronegativity':3.98, 'charges':{'magic': 10}}
+	,10: {'name': 'NONAME', 'symbol': 'NA', 'mass': 20., 'valence': 18, 'electronegativity':0, 'charges':{'magic': 10}}
+	,11: {'name': 'Natrium', 'symbol': 'Na', 'mass': 23., 'valence': 1, 'electronegativity':0.93, 'charges':{'magic': 10}}
+	,12: {'name': 'NONAME', 'symbol': 'NA', 'mass': 24., 'valence': 2, 'electronegativity':1.31, 'charges':{'magic': 10}}
+	,13: {'name': 'Adamantium', 'symbol': 'Ad', 'mass': 27., 'valence': 13, 'electronegativity':1.61, 'charges':{'magic': 10}}
+	,14: {'name': 'Flintium', 'symbol': 'Fn', 'mass': 28., 'valence': 14, 'electronegativity':1.90, 'charges':{'magic': 10}}
+	,15: {'name': 'Aphroditium', 'symbol': 'Ap', 'mass': 31., 'valence': 15, 'electronegativity':2.19, 'charges':{'magic': 10}}
+	,16: {'name': 'Thionium', 'symbol': 'Th', 'mass': 32., 'valence': 16, 'electronegativity':2.58, 'charges':{'magic': 10}}
+	,17: {'name': 'Haloginium', 'symbol': 'Ha', 'mass': 35., 'valence': 17, 'electronegativity':2.16, 'charges':{'magic': 10}}
+	,18: {'name': 'NONAME', 'symbol': 'NA', 'mass': 40., 'valence': 18, 'electronegativity':0, 'charges':{'magic': 10}}
+	,19: {'name': 'Kalium', 'symbol': 'K', 'mass': 40., 'valence': 1, 'electronegativity':0.82, 'charges':{'magic': 10}}
+	,20: {'name': 'NONAME', 'symbol': 'NA', 'mass': 40., 'valence': 2, 'electronegativity':1.00, 'charges':{'magic': 10}}
+	,21: {'name': 'NONAME', 'symbol': 'NA', 'mass': 40., 'valence': 3, 'electronegativity':1.36, 'charges':{'magic': 10}}
+	,22: {'name': 'Zeusium', 'symbol': 'Z', 'mass': 48., 'valence': 4, 'electronegativity':1.54, 'charges':{'magic': 10}}
+	,23: {'name': 'NONAME', 'symbol': 'Z', 'mass': 48., 'valence': 5, 'electronegativity':1.63, 'charges':{'magic': 10}}
+	,24: {'name': 'NONAME', 'symbol': 'Z', 'mass': 48., 'valence': 6, 'electronegativity':1.66, 'charges':{'magic': 10}}
+	,25: {'name': 'Orichalcium', 'symbol': 'Or', 'mass': 55., 'valence': 7, 'electronegativity':1.55, 'charges':{'magic': 10}}
+	,26: {'name': 'Ferrium', 'symbol': 'Fe', 'mass': 56., 'valence': 8, 'electronegativity':1.83, 'charges':{'magic': 10}}		
+	,27: {'name': 'NONAME', 'symbol': 'NA', 'mass': 1.008, 'valence': 9, 'electronegativity':1.88, 'charges':{'magic': 10}}
+	,28: {'name': 'NONAME', 'symbol': 'NA', 'mass': 1.008, 'valence': 10, 'electronegativity':1.91, 'charges':{'magic': 10}}
+	,29: {'name': 'Cuprium', 'symbol': 'Cu', 'mass': 64., 'valence': 11, 'electronegativity':1.90, 'charges':{'magic': 10}}
+	,30: {'name': 'NONAME', 'symbol': 'NA', 'mass': 1.008, 'valence': 12, 'electronegativity':1.65, 'charges':{'magic': 10}}
+	,31: {'name': 'NONAME', 'symbol': 'NA', 'mass': 1.008, 'valence': 13, 'electronegativity':1.81, 'charges':{'magic': 10}}
+	,32: {'name': 'NONAME', 'symbol': 'NA', 'mass': 1.008, 'valence': 14, 'electronegativity':2.01, 'charges':{'magic': 10}}
+	,33: {'name': 'Zarnikium', 'symbol': 'Zr', 'mass': 75., 'valence': 15, 'electronegativity':2.18, 'charges':{'magic': 10}}
+	,34: {'name': 'Lunium', 'symbol': 'Lu', 'mass': 79., 'valence': 16, 'electronegativity':2.55, 'charges':{'magic': 10}}
+	,35: {'name': 'Murium', 'symbol': 'Mu', 'mass': 80., 'valence': 17, 'electronegativity':2.96, 'charges':{'magic': 10}}
+	,36: {'name': 'Kryptonium', 'symbol': 'NA', 'mass': 84., 'valence': 18, 'electronegativity':3.00, 'charges':{'magic': 10}}
+	,47: {'name': 'Argentium', 'symbol': 'Ag', 'mass': 107., 'valence': 11, 'electronegativity':1.93, 'charges':{'magic': 10}}
+	,50: {'name': 'Stannium', 'symbol': 'Sn', 'mass': 119., 'valence': 14, 'electronegativity':1.96, 'charges':{'magic': 10}}
+	,51: {'name': 'Stibium', 'symbol': 'Sb', 'mass': 122., 'valence': 15, 'electronegativity':2.05, 'charges':{'magic': 10}}
+	,79: {'name': 'Aurium', 'symbol': 'Au', 'mass': 197., 'valence': 11, 'electronegativity':2.54, 'charges':{'magic': 10}}
+	,80: {'name': 'Hermesium', 'symbol': 'Hm', 'mass': 201., 'valence': 12, 'electronegativity':2.00, 'charges':{'magic': 10}}
+	,82: {'name': 'Plumbium', 'symbol': 'Pb', 'mass': 207., 'valence': 14, 'electronegativity':1.87, 'charges':{'magic': 10}}
+	,92: {'name': 'Caelium', 'symbol': 'Ux', 'mass': 238., 'valence': 0, 'electronegativity':1.38, 'charges':{'magic': 10}}
+	,94: {'name': 'Hadium', 'symbol': 'Uz', 'mass': 244., 'valence': 0, 'electronegativity':1.28, 'charges':{'magic': 10}}
+}
+
+import json
+
+
+class Particle:
+	"""
+	Particle.
+
+	TODO: Longer class definition.
+
+	Attributes:
+		atomic_number: An integer representing the number of protons in this
+			particle.
+		name: A string of the name of this element.
+		symbol: A two character string of this element's symbol.
+		mass: A float indicating the molar mass of this particle.
+		valence: An int indicating the valence number of this particle.
+		electronegativity: A float indicating the electronegativity of this
+			particle.
+		charges: A dict mapping a string charge name to a float charge value.
+	"""
+
+	def __init__(self, atomic_number):
+		element = element_database.get(atomic_number)
+		assert element is not None, 'Atomic number {} undiscovered!'.format(atomic_number)
+		self.atomic_number = atomic_number
+		self.name = element['name']
+		self.symbol = element['symbol']
+		self.mass = element['mass']
+		self.valence = element['valence']
+		self.electronegativity = element['electronegativity']
+		self.charges = element['charges']
+
+
+	def __repr__(self):
+		return 'Name: {}\n\tAtomic number: {}\n\tSymbol: {}\n\tMass: {}\n\tValence: {}\n\tEN: {}'.format(
+			self.name,
+			self.atomic_number,
+			self.symbol,
+			self.mass,
+			self.valence,
+			self.electronegativity)
+
+		
+
+
+
